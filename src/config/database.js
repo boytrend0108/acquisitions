aa7d8627bd6ba42f83f14b3f0272be130d035bed
boytrend0108 <boytrend@gmail.com>
2025-09-17 08:01:36 +0300

    feat: setup a Neon Postress with a Neon

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+
+const sql = neon(process.env.DATABASE_URL);
+const db = drizzle({ client: sql });
+
+export { db, sql };
